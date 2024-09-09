e5dc2bee912898fbcd19e6229f3faf439ba9b541
Jaqea <1601676692@qq.com>
2024-09-10 00:44:29 +0800

    ✨ feat(custom): add some geometry

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,29 @@ import { Canvas } from '@react-three/fiber';
 export default function App() {
   return (
     <>
-      <div id="canvas-container">
-        <Canvas>
-          <ambientLight intensity={0.1} />
-          <directionalLight color="red" position={[0, 0, 5]} />
-          <mesh>
-            <boxGeometry />
-            <meshStandardMaterial />
+      <div id="canvas-container" className="w-full h-full">
+        <Canvas
+          gl={{ logarithmicDepthBuffer: true, antialias: false }}
+          dpr={[1, 1.5]}
+          camera={{ position: [0, 0, 15], fov: 25 }}
+        >
+          <color attach="background" args={['#15151a']} />
+          <hemisphereLight intensity={0.5} />
+          <mesh
+            scale={4}
+            position={[3, -1.161, -1.5]}
+            rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}
+          >
+            <ringGeometry args={[0.9, 1, 4, 1]} />
+            <meshStandardMaterial color="white" roughness={0.75} />
+          </mesh>
+          <mesh
+            scale={4}
+            position={[-3, -1.161, -1]}
+            rotation={[-Math.PI / 2, 0, Math.PI / 2.5]}
+          >
+            <ringGeometry args={[0.9, 1, 3, 1]} />
+            <meshStandardMaterial color="white" roughness={0.75} />
           </mesh>
         </Canvas>
       </div>
