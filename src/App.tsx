64535415c322d48b6ac1659954153387e4ddb130
Jaqea <1601676692@qq.com>
2024-09-11 00:54:39 +0800

    ✨ feat(custom): add EffectComposer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { Canvas } from '@react-three/fiber';
+import { Environment, Lightformer, OrbitControls } from '@react-three/drei';
+import { Effects } from './Effects';
 
 export default function App() {
   return (
@@ -27,6 +29,80 @@ export default function App() {
             <ringGeometry args={[0.9, 1, 3, 1]} />
             <meshStandardMaterial color="white" roughness={0.75} />
           </mesh>
+          <Environment resolution={512}>
+            {/* Ceiling */}
+            <Lightformer
+              intensity={2}
+              rotation-x={Math.PI / 2}
+              position={[0, 4, -9]}
+              scale={[10, 1, 1]}
+            />
+            <Lightformer
+              intensity={2}
+              rotation-x={Math.PI / 2}
+              position={[0, 4, -6]}
+              scale={[10, 1, 1]}
+            />
+            <Lightformer
+              intensity={2}
+              rotation-x={Math.PI / 2}
+              position={[0, 4, -3]}
+              scale={[10, 1, 1]}
+            />
+            <Lightformer
+              intensity={2}
+              rotation-x={Math.PI / 2}
+              position={[0, 4, 0]}
+              scale={[10, 1, 1]}
+            />
+            <Lightformer
+              intensity={2}
+              rotation-x={Math.PI / 2}
+              position={[0, 4, 3]}
+              scale={[10, 1, 1]}
+            />
+            <Lightformer
+              intensity={2}
+              rotation-x={Math.PI / 2}
+              position={[0, 4, 6]}
+              scale={[10, 1, 1]}
+            />
+            <Lightformer
+              intensity={2}
+              rotation-x={Math.PI / 2}
+              position={[0, 4, 9]}
+              scale={[10, 1, 1]}
+            />
+            {/* Sides */}
+            <Lightformer
+              intensity={2}
+              rotation-y={Math.PI / 2}
+              position={[-50, 2, 0]}
+              scale={[100, 2, 1]}
+            />
+            <Lightformer
+              intensity={2}
+              rotation-y={-Math.PI / 2}
+              position={[50, 2, 0]}
+              scale={[100, 2, 1]}
+            />
+            {/* Key */}
+            <Lightformer
+              form="ring"
+              color="red"
+              intensity={10}
+              scale={2}
+              position={[10, 5, 10]}
+              onUpdate={(self) => self.lookAt(0, 0, 0)}
+            />
+          </Environment>
+          <Effects />
+          <OrbitControls
+            enablePan={false}
+            enableZoom={false}
+            minPolarAngle={Math.PI / 2.2}
+            maxPolarAngle={Math.PI / 2.2}
+          />
         </Canvas>
       </div>
       <h1 className="text-red-700">hello world</h1>
